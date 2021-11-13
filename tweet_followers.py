94662a2cfc7742478413cb3ab65ae70fbdae3027
nikEE2021 <51927797+nikEE2021@users.noreply.github.com>
2021-11-13 01:17:29 -0500

    Add files via upload

--- tweet_followers.py
+++ tweet_followers.py
@@ -10,22 +10,28 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-tweets = api.search_tweets(q='GPU', result_type='popular', lang='en', count=5)
-DF = pd.DataFrame(columns=['tweet_no', 'screen_name', 'id'])
-i = 1
+term = input()
 
-for tweet in tweets:
-    screen_name = tweet.user.screen_name
-    id = tweet.user.id
-    followers = api.get_followers(user_id=id, screen_name=screen_name, count=5)
+def tweetfollow(word):
+    tweets = api.search_tweets(q=word, result_type='popular', lang='en', count=5)
+    DF = pd.DataFrame(columns=['tweet_no', 'screen_name', 'id'])
+    i = 1
 
-    for follower in followers:
-        follower_name = follower.screen_name
-        follower_id = follower.id
-        TWEET_follow = [i, follower_name, follower_id]
-        DF.loc[len(DF)] = TWEET_follow
+    for tweet in tweets:
+        screen_name = tweet.user.screen_name
+        id = tweet.user.id
+        followers = api.get_followers(user_id=id, screen_name=screen_name, count=5)
 
-    i = i + 1
+        for follower in followers:
+            follower_name = follower.screen_name
+            follower_id = follower.id
+            TWEET_follow = [i, follower_name, follower_id]
+            DF.loc[len(DF)] = TWEET_follow
 
-filename = 'tweet_followers.csv'
-DF.to_csv(filename)
+        i = i + 1
+
+    filename = 'tweet_followers.csv'
+    DF.to_csv(filename)
+    return
+
+tweetfollow(term)
